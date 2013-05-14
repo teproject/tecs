640adc758b92519d4eb3ca48e5909fa5dad192e0
Stiliyan Lazarov <sl01303p@pace.edu>
2013-05-14 12:21:11 -0400

    Added database schema to Config/Schema/tecs_schema.sql

--- Config/Schema/tecs_schema.sql
+++ Config/Schema/tecs_schema.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 05, 2013 at 07:30 PM
+-- Generation Time: May 14, 2013 at 06:17 PM
 -- Server version: 5.5.8
 -- PHP Version: 5.3.5
 
@@ -36,14 +36,7 @@ CREATE TABLE IF NOT EXISTS `news` (
   `created_by` int(11) NOT NULL,
   `modified_by` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
-
---
--- Dumping data for table `news`
---
-
-INSERT INTO `news` (`id`, `title`, `photo_file_name`, `content`, `published`, `created`, `modified`, `created_by`, `modified_by`) VALUES
-(13, 'Biomedical Competitions for Student Innovators', 'Open_2011_student_team.jpg', 'The <a href="http://nciia.org/" target="_blank" rel="nofollow">National Collegiate Inventors and Innovators Alliance</a> (NCIIA) will hold their annual Biomedical Competitions for Undergraduate and Graduate students this spring.\r\n\r\nVisit <a href="http://nciia.org/competitions/" target="_blank" rel="nofollow">http://nciia.org/competitions/</a> for more information and enter your idea for chance to win!<br><br><i></i><br>', 'Yes', '2013-02-27 17:49:47', '2013-02-27 23:38:55', 0, 0);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
 
 -- --------------------------------------------------------
 
@@ -62,11 +55,6 @@ CREATE TABLE IF NOT EXISTS `pages` (
   `modified` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `pages`
---
-
 
 -- --------------------------------------------------------
 
@@ -88,16 +76,6 @@ CREATE TABLE IF NOT EXISTS `slides` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
---
--- Dumping data for table `slides`
---
-
-INSERT INTO `slides` (`id`, `title`, `photo_file_name`, `link`, `order`, `published`, `created`, `modified`, `created_by`, `modified_by`) VALUES
-(1, 'Entrepreneurship', 'Entrepreneurship.png', '', 0, 'Yes', '2013-02-27 17:36:39', '2013-02-27 17:36:39', 0, 0),
-(2, 'NCIIA''s Spring 2013 BME Competition', 'nciia.png', 'http://nciia.org/competitions/', 1, 'Yes', '2013-02-05 20:07:34', '2013-02-27 17:01:21', 0, 0),
-(8, 'Kauffman Foundation', 'home_banner_12_2012.jpg', 'http://www.kauffman.org/', 0, 'Yes', '2013-02-27 17:06:15', '2013-02-27 17:06:15', 0, 0),
-(10, 'Entrepreneurship', 'Entrepreneurship2.png', '', 0, 'Yes', '2013-02-27 17:37:01', '2013-02-27 17:37:01', 0, 0);
-
 -- --------------------------------------------------------
 
 --
@@ -114,14 +92,4 @@ CREATE TABLE IF NOT EXISTS `users` (
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `group`, `name`, `email`, `password`, `activated`, `created`, `modified`) VALUES
-(1, 'Administrator', 'Stiliyan Lazarov', 'sl01303p@pace.edu', '1bcd3d3531a98e66e09cf95876b96c7ce803f16a', 'Yes', '2012-12-04 18:37:25', '2012-12-04 18:37:25'),
-(4, 'Member', 'Dr. Wolf', 'ss@test.edu', '877e7532cabe3f351bc345b47ad478cc557edc10', 'Yes', '2012-12-04 20:45:24', '2012-12-04 20:45:24'),
-(5, 'Member', 'Stiliyan Lazarov', 'sl01303n@pace.edu', 'e3986e422008dcec11a37800f5a0a5ce3cec5cc4', 'Yes', '2012-12-18 23:13:52', '2012-12-18 23:13:52'),
-(6, 'Member', 'Dr. Joseph', 'ajoseph2@pace.edu', '3e3195bb6e0ff5a93af2e5efcde0a7d1c75f7a95', 'No', '2013-02-06 18:49:03', '2013-02-06 18:49:03');
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
